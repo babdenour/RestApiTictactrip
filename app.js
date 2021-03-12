a6f144a4e78a7cf3f1655c79d9d9facccbcd54fa
babdenour <abde1802@hotmail.fr>
2021-03-12 22:02:39 +0100

    [justify] starting justify endpoint

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Api running...");
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+	res.send("hello");
+});
+app.get("/api/justify", function (req, res) {
+	res.send("justify");
+});
+
+app.listen(3002, () => {
+	console.log("Server running...");
+});
